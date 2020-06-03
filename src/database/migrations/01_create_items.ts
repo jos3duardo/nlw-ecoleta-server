3b540065f266c1266e8602bbf47702421bc93478
jos3duardo <jos3duardolopes@gmail.com>
2020-06-02 20:38:14 -0400

    finalizando criação das migrations

--- src/database/migrations/01_create_items.ts
+++ src/database/migrations/01_create_items.ts
@@ -1,7 +1,12 @@
-export async function up() {
-
+import Knex from 'knex'
+export async function up(Knex: Knex) {
+    return Knex.schema.createTable('items', table => {
+        table.increments('id').primary();
+        table.string('image').notNullable();
+        table.string('title').notNullable();
+    });
 }
 
-export async function down() {
-
+export async function down(Knex: Knex) {
+    return Knex.schema.dropTable('items');
 }
--- src/database/migrations/02_create_points_items.ts
+++ src/database/migrations/02_create_points_items.ts
@@ -1,7 +1,12 @@
-export async function up() {
-
+import Knex from 'knex'
+export async function up(Knex: Knex) {
+    return Knex.schema.createTable('points_items', table => {
+        table.increments('id').primary();
+        table.integer('point_id').notNullable().references('id').inTable('points')
+        table.integer('item_id').notNullable().references('id').inTable('items')
+    });
 }
 
-export async function down() {
-
+export async function down(Knex: Knex) {
+    return Knex.schema.dropTable('points_items');
 }
